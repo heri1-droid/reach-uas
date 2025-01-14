4c63f904b161f8fabc7a8685a5a823c6271f8be6
heri1-droid <heri.cyber186@gmail.com>
2025-01-14 16:37:25 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   external: ['axios'],
-
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios'] // Menambahkan axios ke daftar external agar tidak dibundel
-    }
-  }
+
 })
+
+
